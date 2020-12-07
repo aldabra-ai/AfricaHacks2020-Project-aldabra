b402c569bb99dad90cf0bac01ec06a1a45bda411
Amid <amidbidee@gmail.com>
2020-12-07 21:48:39 +0100

    database integrated

---
+++ backend/aldabraai/baseapp/models/__init__.py
@@ -0,0 +1,1 @@
+from .staticinfo import StaticAppInfo
---
+++ backend/aldabraai/baseapp/models/staticinfo.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class StaticAppInfo(models.Model):
+    company_name = models.CharField(max_length=150)
+    app_name = models.CharField(max_length=150)
+    webapp_url = models.URLField(max_length=50)
+    date_founded = models.DateField()
+    logo = models.FileField(upload_to='')
+    #address
+
+    def __str__(self):
+        return self.app_name
+    
+    class Meta:
+        verbose_name = 'Static Application Information'
+        verbose_name_plural = 'Static Application Informations'
