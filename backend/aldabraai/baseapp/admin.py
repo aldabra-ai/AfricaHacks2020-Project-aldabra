1fe0d6c563b0c19ae2c60b452b3a1861ac62e139
Amid <amidbidee@gmail.com>
2020-12-07 22:45:40 +0100

    -

--- backend/aldabraai/baseapp/admin.py
+++ backend/aldabraai/baseapp/admin.py
@@ -3,4 +3,4 @@ from .models import StaticAppInfo
 
 @admin.register(StaticAppInfo)
 class StaticAppInfoAdmin(admin.ModelAdmin):
-    list_display = ('company_name', 'app_name', 'web_url', 'date_founded',)
+    list_display = ('company_name', 'app_name', 'webapp_url', 'date_founded',)
