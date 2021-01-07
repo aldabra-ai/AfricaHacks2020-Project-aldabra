02bc6d3b3e3f82e55a44df66fe7ba306b9743630
Aaron Abiodun <amidbidee@gmail.com>
2021-01-07 22:17:39 +0100

    react integration #2

---
+++ aldabraai/frontend/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
