f67af852cc71df8d9c16f4b97a096860b5e0edd5
Aaron Abiodun <amidbidee@gmail.com>
2021-01-16 07:53:27 +0100

    '-'

---
+++ aldabraai/appointment/models/appointment.py
@@ -0,0 +1,11 @@
+# from django.db import models
+# from django.conf import settings
+# from django.utils import timezone 
+
+
+
+# class Appointment(models.Model):
+#     patient_name = models.CharField('Patients Full Name', max_length=500)
+    
+
+
