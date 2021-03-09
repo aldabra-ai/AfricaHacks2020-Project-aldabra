947b64d0fa163f67d6f86dc2ede978c6b40da025
Aaron Abiodun Amid <amidbidee@gmail.com>
2021-03-09 15:06:41 +0100

    Api (#9)
    * '-'
    
    * '-'
    
    * '-'
    
    * '-'
    
    * code refactoring
    
    * '-'
    
    * '-'

--- aldabraai/aldabraai/settings/base.py
+++ aldabraai/aldabraai/settings/base.py
@@ -12,7 +12,15 @@ from django.core.exceptions import ImproperlyConfigured
 with open('secrets.json') as file:
     secrets = json.loads(file.read())   
 
-def get_variable_or_secrete(var_or_setting, secrets=secrets):
+    
+def get_variable_or_secrete(var_or_setting, secrets=secrets, limit_to_secret=False):
+    if limit_to_secret:
+        try:
+            return secrets[var_or_setting]
+        except KeyError:
+            error_msg = f'No secret named {var_or_setting}'
+            raise ImproperlyConfigured(error_msg)
+            
     """Get the environment variable or return exception."""
     try:
         return os.environ[var_or_setting]
